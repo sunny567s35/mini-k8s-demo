636ebd288aa7800311613b4cd92e3543df7b5601
BHEMAL SASI SANDEEP SINGH <66064401+sunny567s35@users.noreply.github.com>
2025-02-28 11:12:20 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -56,4 +56,4 @@ if __name__ == '__main__':
     # Start the Flask application
     port = int(os.environ.get('PORT', 5000))
     print(f"Starting {APP_NAME} v{APP_VERSION} on port {port}")
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port,debug=True)
